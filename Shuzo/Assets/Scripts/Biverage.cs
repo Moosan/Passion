888f15dc31c80beb1a4078c8a663d7aca814bb9d
Fkeito <kei1229tomi@gmail.com>
2019-07-20 16:15:19 +0900

    キュえークワックワックあ

---
+++ Shuzo/Assets/Scripts/Biverage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Biverage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
