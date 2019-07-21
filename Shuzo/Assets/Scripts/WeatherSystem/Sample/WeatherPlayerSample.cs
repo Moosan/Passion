76a1ce6040690e2d0e75e27d97d5403cf1d09f7d
Moosan <washer2016@outlook.jp>
2019-07-21 15:02:49 +0900

    AddSample

---
+++ Shuzo/Assets/Scripts/WeatherSystem/Sample/WeatherPlayerSample.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+namespace Assets.Scripts.WeatherSystem.Sample
+{
+    public class WeatherPlayerSample : WeatherableMonoBehavior
+    {
+        public override void OnSunny()
+        {
+            Debug.Log("Playerが晴れになった");
+        }
+        public override void OnRainy()
+        {
+            Debug.Log("Playerが雨になった");
+        }
+        public override void OnThunder()
+        {
+            Debug.Log("Playerが雷になった");
+        }
+        public override void OnSnowy()
+        {
+            Debug.Log("Playerが雪になった");
+        }
+    }
+}
---
+++ Shuzo/Assets/Scripts/WeatherSystem/Sample/WeatherableObjectSample.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+namespace Assets.Scripts.WeatherSystem.Sample
+{
+    public class WeatherableObjectSample : WeatherableMonoBehavior
+    {
+        public override void OnSunny()
+        {
+            Debug.Log("周りが晴れになった");
+        }
+        public override void OnRainy()
+        {
+            Debug.Log("周りが雨になった");
+        }
+        public override void OnThunder()
+        {
+            Debug.Log("周りが雷になった");
+        }
+        public override void OnSnowy()
+        {
+            Debug.Log("周りが雪になった");
+        }
+    }
+}
