200cdda377e981af5a79e137465872b59e133548
Moosan <washer2016@outlook.jp>
2019-07-21 14:16:43 +0900

    AddWeatherableMonoBehavior

---
+++ Shuzo/Assets/Scripts/WeatherSystem/WeatherableMonoBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+namespace Assets.Scripts.WeatherSystem
+{
+    public abstract class WeatherableMonoBehavior : MonoBehaviour, IWeatherable
+    {
+        public virtual void OnSunny()
+        {
+
+        }
+        public virtual void OnRainy()
+        {
+
+        }
+        public virtual void OnThunder()
+        {
+
+        }
+        public virtual void OnSnowy()
+        {
+
+        }
+    }
+}
